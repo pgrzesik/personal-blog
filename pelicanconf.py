ba6c22560abc2bce50931ca418f6ddf5b8c2d2f0
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-19 17:39:00 +0200

    Adjust menuitems and show menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,11 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Piotr Grzesik'
-SITENAME = 'Piotr Grzesik'
+SITENAME = 'Piotr Grzesik\'s blog'
 SITEURL = ''
+SITETITLE = AUTHOR
+SITESUBTITLE = 'Software developer'
+
 
 THEME = 'flex-theme'
 PATH = 'content'
@@ -20,17 +23,20 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('envelope', 'mailto:pj.grzesik@gmail.com'),
           ('twitter', 'https://twitter.com/p_grzesik'),
           ('github', 'https://github.com/pgrzesik'),
           ('linkedin', 'https://www.linkedin.com/in/piotr-grzesik-a8bb8067'))
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+
+# Menu items
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
 
 DEFAULT_PAGINATION = 10
 
